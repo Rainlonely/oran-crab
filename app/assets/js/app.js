c11145d5260e28e53e33b8d8932904b08bd0d9d4
Rain Wu <rainlonely@me.com>
2016-04-07 11:55:18 +0800

    add samlple cartoon

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
 		var liToTop = $(this).offset().top -80;
 		$('.cursor').css('top',liToTop+'px');
 	});
+
+	var cxn01CartoonShow = function(){
+		$('#cxn01_2').transition({opacity: 1, marginBottom: '40px'});
+		$('#cxn01_3').transition({opacity: 1, marginTop: '0px'});
+	}
+
+	$('a[href="#cxn01"]').on('shown.bs.tab', function (e) {
+  	cxn01CartoonShow();
+	});
+	
 });
